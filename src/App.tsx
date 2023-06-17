ee842b30b1c78711345e0a3a4f5b4dcf1e3c969f
Berkin Anik <berkinanik@gmail.com>
2023-06-17 04:49:22 +0300

    feat: add hash router using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import { Box } from '@chakra-ui/react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import { Navbar } from './components/navbar/Navbar';
+import { HomePage } from './pages/home/HomePage';
 
 export const App = () => {
   return (
     <Box display="flex" width="100%" height="100%" flexDirection="column">
-      <Navbar />
-      <Box
-        display="flex"
-        maxWidth="1200px"
-        padding={4}
-        marginX="auto"
-        width="100%"
-      >
-        Content
-      </Box>
+      <HashRouter>
+        <Navbar />
+
+        <Box
+          display="flex"
+          maxWidth="1200px"
+          padding={4}
+          marginX="auto"
+          width="100%"
+        >
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+
+            <Route path="/test">
+              <Box>Test</Box>
+            </Route>
+
+            <Route path="*">
+              <Box>404</Box>
+            </Route>
+          </Switch>
+        </Box>
+      </HashRouter>
     </Box>
   );
 };
