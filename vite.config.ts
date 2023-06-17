a89b0b64f7370ae0fdb2f114c4cc54454eccdc3b
Berkin Anik <berkinanik@gmail.com>
2023-06-17 04:29:18 +0300

    feat: add path alias configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 /* eslint-disable import/no-default-export */
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
